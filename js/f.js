ed6383dadd7da48acaed03218c766af2013417db
brunojosealvessantos <brunosantosengmec@gmail.com>
2019-12-12 21:50:59 -0300

    Armazenamento do codigo fonte do programa

---
+++ js/f.js
@@ -0,0 +1,14 @@
+function limparCampos(){
+
+
+    document.getElementById("inNome").value = "";
+    document.getElementById("rbMasculino").checked = false;
+    document.getElementById("rbFeminino").checked = false;
+    document.getElementById("inAltura").value = "";
+    document.getElementById("outResposta").textContent = "";
+    document.getElementById("inNome").focus();
+}
+
+
+var btLimpar = document.getElementById("btLimpar");
+btLimpar.addEventListener("click", limparCampos);
